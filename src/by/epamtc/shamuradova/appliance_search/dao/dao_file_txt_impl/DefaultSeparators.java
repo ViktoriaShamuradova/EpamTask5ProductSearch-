0d5ce2c77fcadad3fb9f7f31f621bd41b0d3f33f
Viktoria Shamuradova <viktoriashamuradova@gmail.com>
2020-07-21 08:12:50 +0300

    Default separators

---
+++ src/by/epamtc/shamuradova/appliance_search/dao/dao_file_txt_impl/DefaultSeparators.java
@@ -0,0 +1,8 @@
+package by.epamtc.shamuradova.appliance_search.dao.dao_file_txt_impl;
+
+public class DefaultSeparators {
+    public static final String FOR_APPLIANCE_NAME = ":";
+    public static final String BETWEEN_PROPERTIES = ", ";
+    public static final String FOR_VALUES_OF_PROPERTIES = "=";
+
+}
